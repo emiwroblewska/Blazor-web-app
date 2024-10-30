2162970c0fd59de64f754520246bb9c81b3b4fdd
Emilia Wróblewska <emiliamwroblewska@gmail.com>
2024-10-30 18:16:25 +0100

    Add list editing options

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -18,9 +18,9 @@ public class GamesClient
 
     public void AddGame(GameDetails game)
     {
-        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
-        var genreName = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
-        var gameSumary = new GameSummary {
+        Genre genreName = GetGenreById(game.GenreId);
+        var gameSumary = new GameSummary
+        {
             Id = games.Count + 1,
             Name = game.Name,
             Genre = genreName.Name,
@@ -28,5 +28,50 @@ public class GamesClient
             ReleaseDate = game.ReleaseDate
         };
         games.Add(gameSumary);
+    }
+
+    public GameDetails GetGame(int id)
+    {
+        GameSummary game = GetGameSummaryById(id);
+        var genre = genres.Single(genre => string.Equals(game.Genre, genre.Name, StringComparison.OrdinalIgnoreCase));
+
+        return new GameDetails
+        {
+            Id = game.Id,
+            Name = game.Name,
+            GenreId = genre.Id.ToString(),
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+    }
+
+    public void UpdateGame(GameDetails updatedGame)
+    {
+        var genre = GetGenreById(updatedGame.GenreId);
+        GameSummary existingGame = GetGameSummaryById(updatedGame.Id);
+
+        existingGame.Name = updatedGame.Name;
+        existingGame.Genre = genre.Name;
+        existingGame.Price = updatedGame.Price;
+        existingGame.ReleaseDate = updatedGame.ReleaseDate;
+    }
+
+    public void DeleteGame(int id)
+    {
+        var game = GetGameSummaryById(id);
+        games.Remove(game);
+    }
+
+    private GameSummary GetGameSummaryById(int id)
+    {
+        var game = games.Find(game => game.Id == id);
+        ArgumentNullException.ThrowIfNull(game);
+        return game;
+    }
+
+    private Genre GetGenreById(string? id)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(id);
+        return genres.Single(genre => genre.Id == int.Parse(id));
     }
 }
--- GameStore.Frontend/Models/GameDetails.cs
+++ GameStore.Frontend/Models/GameDetails.cs
@@ -1,13 +1,22 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Frontend.Models;
 
 public class GameDetails
 {
     public int Id { get; set; }
+
+    [Required]
+    [StringLength(50)]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "The Genre field is required.")]
     public string? GenreId { get; set; }
+
+    [Range(1,1000)]
     public decimal Price { get; set; }
+
     public DateOnly ReleaseDate { get; set; }
 }
 
