c0347f21a6039c451f243d0e1377606f175f60ee
Emilia Wróblewska <emiliamwroblewska@gmail.com>
2024-10-30 13:46:28 +0100

    Add new game page

--- GameStore.Frontend/Clients/GamesClient.cs
+++ GameStore.Frontend/Clients/GamesClient.cs
@@ -12,5 +12,21 @@ public class GamesClient
         new() { Id = 3, Genre = "Sports", Name = "FIFA 23", Price = 69.99M, ReleaseDate = new DateOnly(2022, 9, 27) }
     ];
 
+    private readonly Genre[] genres = new GenresClient().GetGenres();
+
     public GameSummary[] GetGames() => [.. games];
+
+    public void AddGame(GameDetails game)
+    {
+        ArgumentException.ThrowIfNullOrWhiteSpace(game.GenreId);
+        var genreName = genres.Single(genre => genre.Id == int.Parse(game.GenreId));
+        var gameSumary = new GameSummary {
+            Id = games.Count + 1,
+            Name = game.Name,
+            Genre = genreName.Name,
+            Price = game.Price,
+            ReleaseDate = game.ReleaseDate
+        };
+        games.Add(gameSumary);
+    }
 }
