297b87f4000ae61e67798f5e93844494b27a5518
Emilia Wróblewska <emiliamwroblewska@gmail.com>
2024-11-04 13:18:46 +0100

    Endpoints implementationa and input validation

--- GameStore.Api/Dtos/UpdateGameDto.cs
+++ GameStore.Api/Dtos/UpdateGameDto.cs
@@ -1,10 +1,11 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace GameStore.Api.Dtos;
 
-public record class CreateGameDto(
+public record class UpdateGameDto(
     [Required][StringLength(50)] string Name, 
     [Required][StringLength(20)] string Genre, 
-    [Range(1, 100)] decimal Price, 
+    [Range(1, 100)] decimal Price,
     DateOnly ReleaseDate
 );
--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,24 +1,10 @@
 using GameStore.Api.Dtos;
+using GameStore.Api.Endpoints;
 
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-List<GameDto> games = [
-    new( 1, "Fighting", "Street Fighter II", 19.99M, new DateOnly(1992, 7, 15) ),
-    new( 2, "RPG", "Final Fantasy XIV", 59.99M, new DateOnly(2010, 9, 30) ),
-    new( 3, "Sports", "FIFA 23", 69.99M, new DateOnly(2022, 9, 27) )
-];
-
-app.MapGet("games", () => games);
-
-app.MapGet("games/{id}", (int id) => games.Find(game => game.Id == id)).WithName("GetGame");
-
-app.MapPost("games", (CreateGameDto newGame) => 
-{
-    GameDto game = new(games.Count + 1, newGame.Name, newGame.Genre, newGame.Price, newGame.ReleaseDate);
-    games.Add(game);    
-    return Results.CreatedAtRoute("GetGame", new {id = game.Id});
-});
+app.MapGamesEndpoints();
 
 app.Run();
